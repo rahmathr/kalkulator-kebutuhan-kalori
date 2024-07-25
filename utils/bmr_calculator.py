a3d34d1c71aa4e1e0e7e1b1be8aff9eb4925bff0
rahmathr <rahmathr.king@gmail.com>
2024-07-25 22:00:13 +0700

    big update

---
+++ utils/bmr_calculator.py
@@ -0,0 +1,7 @@
+def pria(berat_badan, tinggi_badan, usia):
+  bmr_pria = 66.5 + (13.7 * berat_badan) + (5 * tinggi_badan) - (6.8 * usia)
+  return bmr_pria
+
+def wanita(berat_badan, tinggi_badan, usia):
+  bmr_wanita = 655 + (9.6 * berat_badan) + (1.8 * tinggi_badan) - (4.7 * usia)
+  return bmr_wanita
